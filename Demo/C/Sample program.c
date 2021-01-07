d9429e1100bcf5967c92f709ce7b0f7589c55754
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-07 19:19:06 +0600

    Demo C file

---
+++ Demo/C/Sample program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+int n;
+char ch;
+printf("This is a normal c Program\n");
+printf("Hello world !!!\n");
+
+Printf("Enter Your Name: ");
+scanf("%ch", &ch);
+
+Printf("\nEnter Your Age: ");
+scanf("%d", &n);
+
+retuen 0;
+}
