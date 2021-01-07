d7794f9da20cf064eacbaa2802d811bd71405f36
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-07 19:22:23 +0600

    Demo C++ File

---
+++ Demo/C++/Sample program.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std'
+
+int main()
+{
+
+int n;
+char ch;
+cout<<"This is a normal c Program"<<endl;
+cout<<"Hello world !!!"<<endl;
+
+Cout<<"Enter Your Name: ";
+cin>>ch;
+
+cout"\nEnter Your Age: ";
+cin>>n;
+
+retuen 0;
+}
