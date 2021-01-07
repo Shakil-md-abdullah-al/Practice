2da6073c116d43899942606c783cc3ce381537c6
Md. Abdulllah Al Sahkil <65440571+Shakil-md-abdullah-al@users.noreply.github.com>
2021-01-07 19:32:16 +0600

    Sample GO File

---
+++ Demo/GO/Sample program.go
@@ -0,0 +1,14 @@
+// Go program to print Hello World!
+ 
+package main
+ 
+    import "fmt"
+ 
+    // Main function
+    func
+    main()
+{
+ 
+    // prints Hello World
+    fmt.Println("!... Hello World ...!")
+}
